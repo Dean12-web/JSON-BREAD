f9b8591ecc792951d9694b489964acb5c65db0b7
mahyudin akbar <mahyudinakbar10@gmail.com>
2023-03-27 10:54:06 +0700

    2nd commit

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express')
+// Node.js body parsing middleware.
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const bodyParser = require('body-parser')
